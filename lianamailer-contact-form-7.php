ac703da5f80f8da8f5f7b84437c9fcb7c9a287a0
Timo Pohjanvirta <timo.pohjanvirta@viidakko.fi>
2022-09-29 11:29:24 +0200

    Refactoring code pretty hard because of WP coding standards

--- lianamailer-contact-form-7.php
+++ lianamailer-contact-form-7.php
@@ -15,38 +15,42 @@
  *
  * PHP Version 7.4
  *
- * @category Components
- * @package  WordPress
- * @author   Liana Technologies <websites@lianatech.com>
- * @author   Timo Pohjanvirta <timo.pohjanvirta@lianatech.com>
+ * @package  LianaMailer
  * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
 
 namespace CF7_LianaMailer;
 
+define( 'LMCF7_VERSION', '1.0.51' );
+
 add_action( 'plugins_loaded', '\CF7_LianaMailer\load_plugin', 10, 0 );
 
-function load_plugin() {
-	// if Contact Form 7 is installed (and active?)
+
+/**
+ * Loads LianaMailer - Contact Form 7 plugin
+ *
+ * @return void
+ */
+function load_plugin():void {
+	// if Contact Form 7 is installed (and active?).
 	if ( defined( 'WPCF7_VERSION' ) ) {
 
-		require_once dirname(__FILE__) . '/includes/Mailer/Rest.php';
-		require_once dirname(__FILE__) . '/includes/Mailer/LianaMailerConnection.php';
+		require_once dirname( __FILE__ ) . '/includes/Mailer/class-rest.php';
+		require_once dirname( __FILE__ ) . '/includes/Mailer/class-lianamailerconnection.php';
 
-		// Plugin for Contact Form 7 to add tab for setting mailer settings
-		require_once dirname(__FILE__) . '/includes/LianaMailerPlugin.php';
+		// Plugin for Contact Form 7 to add tab for setting mailer settings.
+		require_once dirname( __FILE__ ) . '/includes/class-lianamailerplugin.php';
 
 		try {
-			$lmPlugin = new LianaMailerPlugin();
-		} catch( \Exception $e ) {
+			$lm_plugin = new LianaMailerPlugin();
+		} catch ( \Exception $e ) {
 			$error_messages[] = 'Error: ' . $e->getMessage();
 		}
-
 
 		/**
 		 * Include admin menu & panel code
 		 */
-		require_once dirname(__FILE__) . '/admin/lianamailer-admin.php';
+		require_once dirname( __FILE__ ) . '/admin/class-lianamailer-contactform7.php';
 	}
 }
