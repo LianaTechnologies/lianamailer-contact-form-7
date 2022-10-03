13ec36defd6a14a8f5eeee753a19b47897a71289
Jaakko Pero <jaakko.pero@lianatech.com>
2022-10-03 10:40:44 +0300

    removed optional plugin URI

--- lianamailer-contact-form-7.php
+++ lianamailer-contact-form-7.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Plugin Name:       LianaMailer - Contact Form 7
- * Plugin URI:        https://www.lianatech.com/solutions/websites
  * Description:       LianaMailer for Contact Form 7.
  * Version:           1.0.51
  * Requires at least: 5.2
